8889b51f67bfa455278ab2353a8f580b3c3f62a5
04arben <b00168185@mytudublin.ie>
2025-12-05 23:46:28 +0000

    Customised the home screen. Added header and black area above it so app can be used with floating camera screens. Added fnaf.jpg to the home screen.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,60 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+
+      <View style={styles.topSpace} />
+
+      <View style={styles.header}>
+        <Text style={styles.title}>Welcome to CineStar</Text>
+        <Text style={styles.subtitle}>Discover trending movies and earn rewards</Text>
+      </View>
+
+
+      <View style={styles.imageContainer}>
+        <Image 
+          source={require("../assets/fnaf.jpg")} 
+          style={styles.image} 
+          resizeMode="contain"
+        />
+      </View>
+
+
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#26297dff",
-    justifyContent: "center",
+    backgroundColor: "#121212",
+  },
+  topSpace: {
+    height: 60,       
+    backgroundColor: "#000", 
+  },
+  header: {
+    marginTop: 20,
+    marginBottom: 20,
+    paddingHorizontal: 20, 
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 4,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#ccc",
+  },
+  imageContainer: {
     alignItems: "center",
+    marginVertical: 20,
+  },
+  image: {
+    width: 500,   
+    height: 250,  
   },
 });
